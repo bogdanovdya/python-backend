258386b33b76214c848a725cec3a0362eb499698
bogdanovdya <40484332+bogdanovdya@users.noreply.github.com>
2018-11-14 13:46:35 +0500

    пара запросов

--- Lesson7/uploader.py
+++ Lesson7/uploader.py
@@ -1,6 +1,20 @@
-from Lesson7.mysql_connector import MySQLConnect
+import pymongo
+import json
+from datetime import datetime, date, time
+
+mng_client = pymongo.MongoClient('localhost', 27017)
+mng_db = mng_client['local']
+db_cm = mng_db['my_collection']
+
+
+def pagination(page_size, c_skip=0):
+    if c_skip > 0:
+        db_cm.find().skip(c_skip).limit(page_size)
+        return pagination(page_size, (c_skip+page_size))
+    else:
+        db_cm.find().limit(page_size)
+        return pagination(page_size, page_size)
 
 
 if __name__ == '__main__':
-    MySQLConnect.upload_table_from_csv('data/yellow_tripdata_2018-01.csv')
-
+    pagination(100)
